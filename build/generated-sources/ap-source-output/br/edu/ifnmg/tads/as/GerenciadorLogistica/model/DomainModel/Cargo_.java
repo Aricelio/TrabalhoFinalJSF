24f31467d9aa421643dae1f5c480cd507e45a7d3
Aricelio <ariceliosouza@hotmail.com>
2014-06-08 10:46:04 -0300

    Correção de erros no repositório

---
+++ build/generated-sources/ap-source-output/br/edu/ifnmg/tads/as/GerenciadorLogistica/model/DomainModel/Cargo_.java
@@ -0,0 +1,15 @@
+package br.edu.ifnmg.tads.as.GerenciadorLogistica.model.DomainModel;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-06-05T09:39:27")
+@StaticMetamodel(Cargo.class)
+public class Cargo_ { 
+
+    public static volatile SingularAttribute<Cargo, String> nome;
+    public static volatile SingularAttribute<Cargo, Long> id;
+    public static volatile SingularAttribute<Cargo, String> descricao;
+
+}
